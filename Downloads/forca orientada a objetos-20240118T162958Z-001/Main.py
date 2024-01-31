198dcb846ae7c65bc5e5971686548cd777be351d
Bruna Machado Porto <139787548+poatekbp@users.noreply.github.com>
2024-01-31 14:22:20 -0300

    Update Main.py

--- Downloads/forca orientada a objetos-20240118T162958Z-001/Main.py
+++ Downloads/forca orientada a objetos-20240118T162958Z-001/Main.py
@@ -1,35 +1,25 @@
 from Hangman import Hangman
 import random
 
-class Main:
-
-    def __init__(self):            
-        #Instantiating the Hangman object
-        self.hangman = Hangman()
-
-    def play(self):
-        #Calling the play function
-        self.hangman.play()
-
 if __name__ == "__main__":
-    game = Main()
+    game = Hangman()
     game.play()
     word_list = game.hangman.read_file()
-    word = game.hangman.define_word(word_list)
+    word = game.define_word(word_list)
     trials = (len(word) * 2)
-    hidden_word = game.hangman.anonymize_word(word)
+    hidden_word = game.anonymize_word(word)
     corrects = list(hidden_word)
 
-    while (game.hangman.guessed == False or trials >= 1):
+    while (game.guessed == False or trials >= 1):
         
         guess = input("Provide a letter: ")
-        game.hangman.check_corrects(word, corrects, guess)
+        verify = game.check_corrects(word, corrects, guess)
 
         if (len(guess) > len(word)):
             guess = input("Provide a valid letter: ")
         
         elif guess == word or corrects == list(word):
-            game.hangman.guessed = True
+            game.guessed = True
             print("Congratulations, you got it right!")
             break
         else: 
